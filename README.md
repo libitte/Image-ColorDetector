1a89c93fa5b6824a1ee981144805c543fc1fd117
libitte <libitte3@gmail.com>
2013-12-08 16:19:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Image::ColorDetector - It's new $module
+
+# SYNOPSIS
+
+    use Image::ColorDetector;
+
+# DESCRIPTION
+
+Image::ColorDetector is ...
+
+# LICENSE
+
+Copyright (C) libitte.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+libitte <libitte3@gmail.com>
---
+++ lib/Image/ColorDetector.pm
@@ -0,0 +1,39 @@
+package Image::ColorDetector;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Image::ColorDetector - It's new $module
+
+=head1 SYNOPSIS
+
+    use Image::ColorDetector;
+
+=head1 DESCRIPTION
+
+Image::ColorDetector is ...
+
+=head1 LICENSE
+
+Copyright (C) libitte.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+libitte E<lt>libitte3@gmail.comE<gt>
+
+=cut
+
